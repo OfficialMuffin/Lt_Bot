2c02e536c630f19ca4c47856b227afced484455d
OfficialMuffin <OfficialMuffin@protonmail.com>
2018-11-12 12:02:37 +0000

    Update __main__.py

---
+++ __main__.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.client
+client = commands.Bot(command_prefix = "!")
+
+# Message to tell you when the bot is ready
+@client.event
+async def on_ready() :
+     print("Bot is Ready!")
+
+
+# Normal Text Chat
+@client.event
+async def on_message(message) :
+     if message.content.upper() == ("HI" || "HELLO") :
+         await client.send_message(message.channel, "Hello There!")
+     if message.content.upper() == ("ARE YOU OK?") :
+         await client.send_message(message.channel, "Yup, I'm great thanks :)")
+
+
+# Client Secret API Key
+client.run("YOUR KEY HERE")
